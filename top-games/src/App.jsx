18f963baddc7d4a37c22a257f4e9a597acc77d55
kimsyd <kimsyd@hawaii.edu>
2023-11-27 10:57:51 -1000

    edit file structure

---
+++ top-games/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Row, Col, Container, Navbar } from "react-bootstrap";
+
+const App = () => (
+  <Container>
+    <Col>
+      <Row>
+        <h1 style={{ paddingTop: "30px", paddingBottom: "30px" }}>Exploration of top-selling video games of each year</h1>
+      </Row>
+    </Col>
+  </Container>
+);
+
+export default App;
---
+++ top-games/src/ui/components/plot_component.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import Plotly from 'plotly.js/dist/plotly';
+import createPlotlyComponent from 'react-plotly.js/factory';
+
+const Plot = createPlotlyComponent(Plotly);
+
+const PlotlyComponent = () => {
+  useEffect(() => {
+    // Your Plotly configuration and data
+    const data = [{
+      x: [1, 2, 3, 4],
+      y: [10, 11, 12, 13],
+      type: 'scatter'
+    }];
+
+    const layout = {
+      title: 'SAMPLE',
+    };
+
+    // Render the plot inside the HTML element with the id 'myPlot'
+    Plotly.newPlot('samplePlot', data, layout);
+  }, []); // Empty dependency array means this effect runs once on mount
+
+  return (
+    <div id="plotContainer" style={{ width: "100%" }}>
+      {/* The HTML element with the id 'myPlot' where the Plotly plot will be rendered */}
+      <div id="myPlot"></div>
+    </div>
+  );
+};
+
+export default PlotlyComponent;
