be6185ef95e182c04521f9b5c112bf9acc37d8e8
msumaylo <97885062+msumaylo@users.noreply.github.com>
2023-11-28 18:31:54 -1000

    progress

--- top-games/src/ui/components/plot_component.jsx
+++ top-games/src/ui/components/plot_component.jsx
@@ -2,13 +2,53 @@ import React, { useEffect } from 'react';
 import Plotly from 'plotly.js/dist/plotly';
 import createPlotlyComponent from 'react-plotly.js/factory';
 
+import Papa from 'papaparse';
+
+// testing this papaparse stuff
+let readSalesAllPlat = "";
+
+Papa.parse('https://raw.githubusercontent.com/kimsyd/top_games/main/top-games/src/data/raw_data/video_game_sales_all_platforms.csv',
+  {
+    delimiter: "",	// auto-detect
+    newline: "",	// auto-detect
+    quoteChar: '"',
+    escapeChar: '"',
+    header: true,
+    transformHeader: undefined,
+    dynamicTyping: true,
+    preview: 0,
+    encoding: "",
+    worker: false,
+    comments: false,
+    step: undefined,
+    complete: function(results,file){
+      console.log("Parsing complete:", results, file);
+      readSalesAllPlat = results.data;
+      console.log(readSalesAllPlat[0]);
+    },
+    error: undefined,
+    download: true,
+    downloadRequestHeaders: undefined,
+    downloadRequestBody: undefined,
+    skipEmptyLines: false,
+    chunk: undefined,
+    chunkSize: undefined,
+    fastMode: undefined,
+    beforeFirstChunk: undefined,
+    withCredentials: undefined,
+    transform: undefined,
+    delimitersToGuess: [',', '\t', '|', ';', Papa.RECORD_SEP, Papa.UNIT_SEP],
+    skipFirstNLines: 0
+  });
+
+
 const Plot = createPlotlyComponent(Plotly);
 
 const PlotlyComponent = () => {
   useEffect(() => {
     // Your Plotly configuration and data
     const data = [{
-      x: [1, 2, 3, 4],
+      x: [readSalesAllPlat[0].totalSales, readSalesAllPlat[1].totalSales, readSalesAllPlat[3].totalSales, readSalesAllPlat[4].totalSales],
       y: [10, 11, 12, 13],
       type: 'scatter'
     }];
