ef9729fed33635ad6dc9dddf1bdc9efa40b42bc8
msumaylo <97885062+msumaylo@users.noreply.github.com>
2023-11-28 16:47:38 -1000

    some headway on parsing CSV in for once

--- top-games/src/index.js
+++ top-games/src/index.js
@@ -6,6 +6,46 @@ import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 import { Container, Col, Row, DropdownButton, Dropdown } from 'react-bootstrap';
 import PlotlyComponent from './ui/components/plot_component';
+import Papa from 'papaparse';
+
+// testing this node.js filereading + papaparse stuff
+let readSalesAllPlat = "";
+
+Papa.parse('https://raw.githubusercontent.com/kimsyd/top_games/main/top-games/src/data/raw_data/video_game_sales_all_platforms.csv',
+  {
+    delimiter: "",	// auto-detect
+    newline: "",	// auto-detect
+    quoteChar: '"',
+    escapeChar: '"',
+    header: true,
+    transformHeader: undefined,
+    dynamicTyping: true,
+    preview: 0,
+    encoding: "",
+    worker: false,
+    comments: false,
+    step: undefined,
+    complete: function(results,file){
+      console.log("Parsing complete:", results, file);
+      readSalesAllPlat = results.data;
+      console.log(readSalesAllPlat[0]);
+    },
+    error: undefined,
+    download: true,
+    downloadRequestHeaders: undefined,
+    downloadRequestBody: undefined,
+    skipEmptyLines: false,
+    chunk: undefined,
+    chunkSize: undefined,
+    fastMode: undefined,
+    beforeFirstChunk: undefined,
+    withCredentials: undefined,
+    transform: undefined,
+    delimitersToGuess: [',', '\t', '|', ';', Papa.RECORD_SEP, Papa.UNIT_SEP],
+    skipFirstNLines: 0
+  })
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
